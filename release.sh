554733b5d2e401222dc4df9a4a2554a92ecdeb8c
Nordine Bittich <nordine@bittich.be>
2022-04-03 12:37:25 +0200

    release

--- release.sh
+++ release.sh
@@ -2,18 +2,12 @@
 git checkout main
 git pull
 
-npm version v$1
+npm version $1
 npm i
 git add .
 
-echo "push tag v$1..."
+echo "push tag $1..."
 git push origin v$1
-
-echo "push docker image v$1..."
-
-docker build -t nbittich/website:v$1 .
-docker tag nbittich/website:v$1 nbittich/website:v$1
-docker push nbittich/website:v$1
 
 git push
 
